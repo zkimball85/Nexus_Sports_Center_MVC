ffe2dab49e1ea5871f7ee38e644a41153d54ba61
Zac Kimball <z.kimball85@gmail.com>
2026-08-01 14:25:54 -0700

    Enable EF Core with SQL Server support
    Added EF Core SQL Server and Tools packages to project file. Registered NexusDbContext in DI with SQL Server connection from configuration. Updated appsettings.json with DefaultConnection string.

--- Nexus_Sports_Center_MVC/Program.cs
+++ Nexus_Sports_Center_MVC/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Nexus_Sports_Center_MVC.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<NexusDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
