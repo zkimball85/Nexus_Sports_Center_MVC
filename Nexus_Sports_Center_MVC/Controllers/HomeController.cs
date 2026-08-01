8980abe7abc469e888fca417346b8cc8c572c02e
Zac Kimball <z.kimball85@gmail.com>
2026-08-01 12:26:20 -0700

    Add project files.

---
+++ Nexus_Sports_Center_MVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Nexus_Sports_Center_MVC.Models;
+using System.Diagnostics;
+
+namespace Nexus_Sports_Center_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Nexus_Sports_Center_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nexus_Sports_Center_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
