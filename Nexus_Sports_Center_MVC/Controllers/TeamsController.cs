89fc5a4b2cc3702774af9cf9c69cb1cb7c5ff882
Zac Kimball <z.kimball85@gmail.com>
2026-08-04 16:11:54 -0700

    Enhance Teams: venue selection, roster view, add player
    - Populate venue SelectList in Teams/Create GET and POST actions
    - Redirect to Roster after team creation; handle validation errors
    - Add Roster action with TeamRosterViewModel for team players
    - Implement AddPlayerToRoster to add players and handle errors

--- Nexus_Sports_Center_MVC/Controllers/TeamsController.cs
+++ Nexus_Sports_Center_MVC/Controllers/TeamsController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Nexus_Sports_Center_MVC.Models;
 using Nexus_Sports_Center_MVC.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nexus_Sports_Center_MVC.ViewModels;
 
 namespace Nexus_Sports_Center_MVC.Controllers;
 
@@ -41,22 +43,23 @@ public class TeamsController : Controller
     // GET: TEAMS/Create
     public IActionResult Create()
     {
+        ViewData["VenueId"] = new SelectList(_context.Venues, "Id", "Name");
         return View();
     }
 
     // POST: TEAMS/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,Name")] Team team)
+    public async Task<IActionResult> Create([Bind("Id,Name,VenueId")] Team team)
     {
         if (ModelState.IsValid)
         {
             _context.Add(team);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return RedirectToAction(nameof(Roster), new { id = team.Id });
         }
+        ViewData["VenueId"] = new SelectList(_context.Venues, "Id", "Name", team.VenueId);
         return View(team);
     }
 
@@ -109,6 +112,43 @@ public class TeamsController : Controller
             return RedirectToAction(nameof(Index));
         }
         return View(team);
+    }
+
+    // GET: TEAMS/Roster/5
+    public async Task<IActionResult> Roster(int? id)
+    {
+        if(id == null) return NotFound();
+
+        var team = await _context.Teams
+            .Include(t => t.HomeVenue)
+            .FirstOrDefaultAsync(m => m.Id == id);
+
+        if (team == null) return NotFound();
+
+        var viewModel = new TeamRosterViewModel
+        {
+            Team = team,
+            ExistingPlayers = await _context.Players.Where(p => p.TeamId == id).ToListAsync(),
+            NewPlayer = new Player { TeamId = team.Id }
+        };
+
+        return View(viewModel);
+    }
+
+    public async Task<IActionResult> AddPlayerToRoster(TeamRosterViewModel viewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Players.Add(viewModel.NewPlayer);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Roster), new { id = viewModel.NewPlayer.TeamId });
+        }
+
+        viewModel.Team = await _context.Teams.FindAsync(viewModel.NewPlayer.TeamId);
+        viewModel.ExistingPlayers = await _context.Players.Where(p => p.TeamId == viewModel.NewPlayer.TeamId).ToListAsync();
+
+        return View("Roster", viewModel);
     }
 
     // GET: TEAMS/Delete/5
