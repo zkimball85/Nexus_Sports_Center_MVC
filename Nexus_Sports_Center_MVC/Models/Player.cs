0fac0f9ff8019696b15909ac3272587a4b26be4f
Zac Kimball <z.kimball85@gmail.com>
2026-08-04 16:14:37 -0700

    Make Player.FirstName/LastName non-nullable strings
    Removed nullable reference types from FirstName and LastName in the Player class. Both properties are now required non-nullable strings, ensuring they always have a value.

--- Nexus_Sports_Center_MVC/Models/Player.cs
+++ Nexus_Sports_Center_MVC/Models/Player.cs
@@ -20,14 +20,14 @@ public class Player
     /// </summary>
     [Required]
     [StringLength(100, MinimumLength = 2)]
-    public string? FirstName { get; set; }
+    public string FirstName { get; set; }
 
     /// <summary>
     /// Gets or sets the last name of the player.
     /// </summary>
     [Required]
     [StringLength(100, MinimumLength = 2)]
-    public string? LastName { get; set; }
+    public string LastName { get; set; }
 
     /// <summary>
     /// Gets or sets the jersey number of the player.
